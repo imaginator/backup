76ea1f32580a9461fa3d2c9cecdd7266d1d6d962
Simon Tennant <simon@imaginator.com>
2022-05-12 20:28:08 +0200

    actually use our snapshots

--- backup.sh
+++ backup.sh
@@ -13,13 +13,13 @@ umount /media/auto-backup-disk-mounted-subvolume-root
 
 mount -t btrfs -o subvol=home   /dev/disk/by-label/auto-backup-disk-filesystem /media/auto-backup-disk-mounted-subvolume-home
   btrfs subvolume snapshot -r /home /home/.snapshots/backup-script-snapshot 
-    ionice -c 3 rsync --delete --delete-excluded --inplace --no-whole-file -aAvx /home /media/auto-backup-disk-mounted-subvolume-home --exclude=".snapshot/" 
+    ionice -c 3 rsync --delete --delete-excluded --inplace --no-whole-file -aAvx /home/.snapshots/backup-script-snapshot /media/auto-backup-disk-mounted-subvolume-home --exclude=".snapshot/" 
   btrfs subvolume delete /home/.snapshots/backup-script-snapshot 
 umount /media/auto-backup-disk-mounted-subvolume-home
 
 mount -t btrfs -o subvol=photos /dev/disk/by-label/auto-backup-disk-filesystem /media/auto-backup-disk-mounted-subvolume-photos
   btrfs subvolume snapshot -r /srv/photos /srv/photos/.snapshots/backup-script-snapshot 
-    ionice -c 3 rsync --delete --delete-excluded --inplace --no-whole-file -aAvx /srv/photos /media/auto-backup-disk-mounted-subvolume-photos --exclude=".snapshot/" 
+    ionice -c 3 rsync --delete --delete-excluded --inplace --no-whole-file -aAvx /srv/photos/.snapshots/backup-script-snapshot /media/auto-backup-disk-mounted-subvolume-photos --exclude=".snapshot/" 
   btrfs subvolume delete /srv/photos/.snapshots/backup-script-snapshot 
 umount /media/auto-backup-disk-mounted-subvolume-photos
 
